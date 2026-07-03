7d8de8d1154558c46971409c26ae2e063868425e
John Calvin <johncalvinsamson@gmail.com>
2026-07-03 22:18:18 +0800

    Added LoginManager

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,13 @@
 
 from flask import render_template, redirect, url_for
 
-from app import app
+from app import app, login_manager
+from app.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    # This looks up the user in your database by their ID
+    return User.query.get(int(user_id))
 
 @app.route('/')
 @app.route('/dashboard')
